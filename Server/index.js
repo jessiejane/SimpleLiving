9485ff4eae84ea0c0734c884c29d07a02d3702ca
selamyihun <selamyihun@gmail.com>
2017-10-03 11:37:20 -0700

    Addded sensorReading request.

--- Server/index.js
+++ Server/index.js
@@ -4,4 +4,14 @@ var app = express();
 var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + '/static'));
-app.listen(3000);
+app.use(bodyParser.json());
+app.listen(3000, function (){
+	console.log('Simpl running on port 3000!')
+});
+
+app.post('/sensorReading', function(req, res) {
+		var itemId = req.body.itemId;
+		var sensorReading = req.body.sensorReading;
+		res.json({ message: 'Got a post /sensorReading request.' + itemId + ' ' + sensorReading});
+	}
+);
