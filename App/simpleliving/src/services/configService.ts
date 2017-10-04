4654aeeb9f32ae73681a08db5c628f74b6de3f12
John Griffin <jhgriffin@wisc.edu>
2017-10-04 06:15:53 -0700

    point to AWS

--- App/simpleliving/src/services/configService.ts
+++ App/simpleliving/src/services/configService.ts
@@ -2,6 +2,6 @@
 
 export class ConfigService {
     getRestEndPointUrl():string {
-        return "localhost:3000/api/"
+        return "http://ec2-13-59-35-127.us-east-2.compute.amazonaws.com:3000/api/"
     }
 }
