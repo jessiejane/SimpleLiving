a7bd3c93fb152e7d3c2ac6c83c2e79848fee5672
Shoua Xiong <shoua_sx@yahoo.com>
2017-10-04 04:07:41 -0500

    update server

--- Server/server.js
+++ Server/server.js
@@ -30,8 +30,8 @@ REST.prototype.connectMysql = function() {
     var pool      =    mysql.createPool({
         connectionLimit : 100,
         host     : 'localhost',
-        user     : 'usr',
-        password : 'usr',
+        user     : 'user',
+        password : 'user',
         database : 'SimpleLiving',
         debug    :  true
     });
