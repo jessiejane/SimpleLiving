e16922916d0e9a33a666501f230369194dcac8d8
kaetoth <kaetoth@gmail.com>
2017-10-04 05:12:22 -0400

    household detail changes

--- App/simpleliving/src/pages/householddetails/householddetails.ts
+++ App/simpleliving/src/pages/householddetails/householddetails.ts
@@ -18,8 +18,27 @@ import { Http, Headers } from '@angular/http';
 export class HouseholdDetailsPage {
   detailSegment: string = "roommates";
   isAndroid: boolean = false;
+  mockNames: string[];
+  mockNumbers: string[];
+  imageThumbnail: string[];
+  items: Array<{mockNames: string, mockNumbers: string, imageThumbnail:string}>;
+
 
   constructor(platform: Platform) {
     this.isAndroid = platform.is('android');
+
+    this.mockNames = ["FirstName LastName", "FirstName LastName2",
+    "FirstName LastName3"]
+    this.mockNumbers = ["123456789","221465798","098765432"]
+    this.imageThumbnail = ["assets/img/imgPlaceHolder.png"]
+
+    this.items = []
+    for (let i = 0; i < this.mockNames.length; i++) {
+      this.items.push({
+        mockNames: this.mockNames[i],
+        mockNumbers: this.mockNumbers[i],
+        imageThumbnail: this.imageThumbnail[0],
+      })
+    }
   }
 }
